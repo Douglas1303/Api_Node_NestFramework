670174928a1d2d083eb399c9367f9ae306534c6f
Douglas1303 <douglassantos6@outlook.com>
2021-05-30 21:52:05 -0300

    Entidade de Produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module'; 
+import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://ApiNodeNest:244005@apinode.lv4wl.mongodb.net/ApiNodeNest'),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '7180',
+      password: '1q2w3e4r!@#$',
+      database: '7180',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [],
   providers: [],
