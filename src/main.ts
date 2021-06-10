3ea5017d103618bff1b4dd333e0431212be02fff
Douglas1303 <douglassantos6@outlook.com>
2021-06-09 23:29:46 -0300

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression'; 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; 
 import { AppModule } from 'src/app.module';
 import { CustomLogger } from './shared/services/logger.service';
 
@@ -9,6 +10,16 @@ async function bootstrap() {
   });
 
   app.use(compression()); 
+
+  // Open API
+  const options = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API node Nest Framework')
+    .setVersion('1.0.0')
+    .addTag('petshop')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
